6f6c1aa723ab842c59f56a3350f610b8a99372b4
Fandy Tsui <fandy@MacBook-Pro-van-Fandy.local>
2020-04-05 17:21:26 +0200

    added NavBar & images

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react';
+import { List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
@@ -13,17 +14,16 @@ const App = () => {
     }, []); // useEffect run one time only
 
     return (
-        <div>
-            <Header as="h2">
-                <Icon name="users" />
-                <Header.Content>Reactivities</Header.Content>
-            </Header>
-            <List>
-                {activities.map((activity) => (
-                    <List.Item key={activity.id}>{activity.title}</List.Item>
-                ))}
-            </List>
-        </div>
+        <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+                <List>
+                    {activities.map((activity) => (
+                        <List.Item key={activity.id}>{activity.title}</List.Item>
+                    ))}
+                </List>
+            </Container>
+        </Fragment>
     );
 };
 
