89e987748926d16dd8dfe6fb4f6996447cbd10a6
Fandy Tsui <fandy@MacBook-Pro-van-Fandy.local>
2020-04-05 20:42:01 +0200

    created ActivityDashboard, ActivityList, AcitivtiyDetails and ActivityForm

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,9 +3,16 @@ import axios from 'axios';
 import { List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+    const [editMode, setEditMode] = useState(false);
+
+    const handleSelectActivity = (id: String) => {
+        setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    };
 
     useEffect(() => {
         axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response) => {
@@ -17,11 +24,13 @@ const App = () => {
         <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-                <List>
-                    {activities.map((activity) => (
-                        <List.Item key={activity.id}>{activity.title}</List.Item>
-                    ))}
-                </List>
+                <ActivityDashboard
+                    activities={activities}
+                    selectActivity={handleSelectActivity}
+                    selectedActivity={selectedActivity}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                />
             </Container>
         </Fragment>
     );
